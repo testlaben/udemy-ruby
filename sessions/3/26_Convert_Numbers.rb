879e491eadd80a3a5cb74b0ebbf3fdd0bcd80b3a
Jørgen Næss <jorgen.ness@gmail.com>
2017-05-06 21:36:51 +0200

    Change folder structure.

---
+++ sessions/3/26_Convert_Numbers.rb
@@ -0,0 +1,33 @@
+# Convert Numbers to Strings and Vice Versa
+
+str = "5"
+p str
+p str.class
+
+p str.to_i.class
+
+p str.to_f
+p str.to_f.class
+
+# Convert number to String
+
+number = 10
+
+p number.class
+p number.to_s
+p number.to_s.class
+
+p number.to_f
+p number.to_f.class
+
+# Convert float to String
+
+pi = 3.14
+
+p pi.class
+
+p pi.to_i
+p pi.to_i.class
+
+p pi.to_s
+p pi.to_s.class
